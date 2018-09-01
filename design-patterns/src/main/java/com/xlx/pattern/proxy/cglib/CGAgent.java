f8ef86c9f7ded8ac49280ef48e7ec6d950e06269
xuelingxiao <xuelinxiao@163.com>
2018-09-01 12:01:12 +0800

    proxy模式,用cglib简单演示

---
+++ design-patterns/src/main/java/com/xlx/pattern/proxy/cglib/CGAgent.java
@@ -0,0 +1,29 @@
+//package com.xlx.pattern.proxy.cglib;
+//
+//
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//import net.sf.cglib.proxy.MethodProxy;
+//
+//import java.lang.reflect.Method;
+//
+///**
+// * 演示 cglib 代理方式
+// */
+//public class CGAgent implements MethodInterceptor {
+//
+//    public Object getInstance(Class clazz) throws Exception{
+//        Enhancer enhancer = new Enhancer();
+//        enhancer.setSuperclass(clazz);
+//        enhancer.setCallback(this);
+//        return enhancer.create();
+//    }
+//
+//    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+//        System.out.println("代理开始了....");
+//        // 这里必须使用方法invokeSuper,invoke会导致循环调用
+//        methodProxy.invokeSuper(o,objects);
+//        System.out.println("代理开始了....");
+//        return null;
+//    }
+//}
---
+++ design-patterns/src/main/java/com/xlx/pattern/proxy/cglib/HongQiCar.java
@@ -0,0 +1,7 @@
+package com.xlx.pattern.proxy.cglib;
+
+public class HongQiCar {
+    public void sale() {
+        System.out.println("卖了一辆红旗");
+    }
+}
